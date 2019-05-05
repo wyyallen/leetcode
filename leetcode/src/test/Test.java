f285986ba3b8ddb106c6ce62b0bc6a3ca703b417
yiwang <wangyi08@kuaishou.com>
2019-05-05 09:22:41 +0800

    init project

---
+++ leetcode/src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author yiwang <wangyi08@kuaishou.com>
+ * Created on 2019-05-05
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("wytest");
+    }
+}
