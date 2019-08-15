7c9e650da10adc9aedc9256559db61a8e63b43f9
wangyi <wangyi08@kuaishou.com>
2019-08-15 09:03:45 +0800

    0815 sort

--- leetcode/src/sort/SortUtils.java
+++ leetcode/src/sort/SortUtils.java
@@ -5,14 +5,71 @@ package sort;
  */
 public class SortUtils {
     /**
-     * 快排
+     * 快排,基准分组排序。
      */
-    public static void quickSort(Number[] numbers){
+    public static void quickSort(int[] arrays) {
 
     }
 
     /**
-     * 冒泡排序
+     *
+     * @param array
+     * @param left
+     * @param right
+     * @return
      */
+    public static int quickSort(int[] array,int left,int right){
+        int temp = array[right];
+        while (left < right){
+            while (left < right && array[left] <= temp){
+                left++;
+            }
+            if (left < right){
+                array[right--] = array[left];
+            }
+            while (left < right && array[right] >= temp){
+                right--;
+            }
+            if (left < right){
+                array[left++] = array[right];
+            }
+        }
+        array[right] = temp;
+        return right;
+    }
 
+    /**
+     * 冒泡排序,相邻交换，稳定
+     */
+    public static void bubbleSort(int[] arrays) {
+        for (int i = 0; i< arrays.length-1; i++) { //排序轮数
+            for (int j = 0; j<arrays.length-i-1;j++) { //比较次数
+                if (arrays[j] > arrays[j+1]) {
+                    int temp = arrays[j];
+                    arrays[j] = arrays[j+1];
+                    arrays[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * 冒泡排序优化，如果一次排序过程中没有发生一次交换，说明该数组已经是有序的了
+     */
+    public static void bubbleSortoptimize(int[] arrays) {
+        boolean flag = true;
+        for (int i = 0; i< arrays.length-1; i++) { //排序轮数
+            for (int j = 0; j<arrays.length-i-1;j++) { //比较次数
+                if (arrays[j] > arrays[j+1]) {
+                    flag = false;
+                    int temp = arrays[j];
+                    arrays[j] = arrays[j+1];
+                    arrays[j+1] = temp;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+    }
 }
---
+++ leetcode/src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * @author yiwang <>
+ * Created on 2019-08-15
+ */
+public class Test {
+
+    public static void show(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ",");
+        }
+        System.out.println();
+    }
+}
